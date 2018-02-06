c689a8953e55904f5970a324f1d3c09e5266dc1f
shubham.chaudhari@capita.co.uk <shubham.chaudhari@capita.co.uk>
2018-02-06 13:30:50 +0530

    Done

--- taskApp-master/src/app/app.component.ts
+++ taskApp-master/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TasksService } from './tasks.service';
 import { Http, Response , Headers, RequestOptions} from '@angular/http';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,10 +21,10 @@ export class AppComponent {
   }
   deleteTasks(id){
     console.log(id);
-    return this.http.get(`https://firestore.googleapis.com/v1beta1/projects/`+id).subscribe(
+    return this.http.delete('https://firestore.googleapis.com/v1beta1/'+id).subscribe(
         data => {
           this.testResponse = data;
-          console.log("Record deleted");
+          alert("Record deleted");
         }
     );
   }
--- taskApp-master/src/app/app.module.ts
+++ taskApp-master/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule
   ],
